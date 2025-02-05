79a5249c27446a1db7f3271bfe15489be6305715
kevindotklein <kevindotklein@gmail.com>
2025-02-05 20:53:42 -0300

    gruvbox

--- init.lua
+++ init.lua
@@ -862,25 +862,27 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'zaki/zazen',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    -- dependencies = { 'rktjmp/lush.nvim' },
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      -- vim.g.zenbones_stark = true
-      vim.cmd.colorscheme 'zazen'
+  { 'ellisonleao/gruvbox.nvim', priority = 1000, config = true },
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
-    end,
-  },
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'zaki/zazen',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   -- dependencies = { 'rktjmp/lush.nvim' },
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     -- vim.g.zenbones_stark = true
+  --     vim.cmd.colorscheme 'zazen'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 
   -- { -- You can easily change to a different colorscheme.
   --   -- Change the name of the colorscheme plugin below, and then
@@ -1057,17 +1059,5 @@ require('lazy').setup({
 
 vim.opt.cursorline = false
 
-vim.g.matchup_enabled = 0
-
-require('mini.pairs').setup {
-  highlight_matching = false,
-}
-
--- for some reason, it's the only way it works :/
-vim.api.nvim_create_autocmd('VimEnter', {
-  callback = function()
-    vim.cmd 'NoMatchParen'
-  end,
-})
-
 vim.opt.tabstop = 4
+vim.cmd 'colorscheme gruvbox'
